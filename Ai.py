2fe88aaa7d686d92706233ae3d22593d629f3190
RRansubhe <94738575+RRansubhe@users.noreply.github.com>
2021-11-22 21:09:52 +0530

    Update Ai.py

--- Ai.py
+++ Ai.py
@@ -1,10 +1,10 @@
-import pyttsx3
 import datetime
+import smtplib
+import webbrowser
+import pyjokes
+import pyttsx3
 import speech_recognition as sr
 import wikipedia
-import webbrowser
-import os
-import  smtplib
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -15,33 +15,51 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
 def wishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour >=0 and hour <12:
-        speak("Good morning Raman")
-    elif hour >=12 and hour < 18:
-        speak("Good afternoon Raman")
+    if 0 <= hour < 12:
+        speak("Good morning")
+    elif 12 <= hour < 18:
+        speak("Good afternoon")
     else:
-        speak("Good evening Raman")
+        speak("Good evening")
     speak("I am your personal assistant. please tell me how may I help you")
 
-def takecommand():
+
+# noinspection PyBroadException
+
+def takeCommand():
+    # It takes microphone input from the user and returns string output
+
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening...")
         r.pause_threshold = 1
+        print("Listening...")
         audio = r.listen(source)
+        try:
+            print("Recognizing...")
+            # Using google for voice recognition.
+            query = r.recognize_google(audio, language='en-in')
+            print(f"User said: {query}\n")  # User query will be printed.
 
-    try:
-        print("Recognising...")
-        query = r.recognize_google(audio, language='en-in')
-        print(f"User said: {query}\n")
+        except Exception as e:
+            # print(e)
+            # Say that again will be printed in case of improper voice
+            print("Say that again please...")
+            takeCommand()
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
 
-    except Exception as e:
-        print("say that again please....")
-        speak("say that again please....")
-        return "None"
-    return query
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service{0}".format(e))
+
+        if query:
+            return query.lower()
+        else:
+            takeCommand()
+
+
 def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
@@ -50,46 +68,44 @@ def sendEmail(to, content):
     server.sendmail("email id", to, content)
     server.close()
 
+
 if __name__ == "__main__":
     wishMe()
     while True:
-        query = takecommand().lower()
+        query = takeCommand()
 
         if 'wikipedia' in query:
             speak("searching wikipedia")
             query = query.replace("wikipedia", "")
             results = wikipedia.summary(query, sentence=2)
             speak("according to wikipedia")
-            print(results)
+            speak(results)
             speak(results)
 
         elif 'open youtube' in query:
             speak("opening youtube")
             webbrowser.open("youtube.com")
 
-        elif 'facebook' in query:
-            speak ("opening facebook")
-            webbrowser.open("facebook.com")
+        elif 'open google' in query:
+            speak("opening google")
+            webbrowser.open("https://www.google.com")
 
-        if 'send mail to find' in query:
-            try:
-                speak("What should I say?")
-                content = takecommand()
-                to = "frnds mail address "
-                sendEmail(to, content)
-                speak("email has been sent")
-            except Exception as e:
-                print(e)
-                speak("sorry Raman. I am not able to send this mail")
+        elif 'open smartschoolz' in query:
+            speak("opening smartschoolz")
+            webbrowser.open("https://smartschoolz.com/login")
 
+        elif 'open codetantra' in query:
+            speak("opening codetantra")
+            webbrowser.open("https://cornerstonepublicschool.codetantra.com/login.jsp")
 
+        elif 'open github' in query:
+            speak("opening ")
+            webbrowser.open("https://github.com/")
 
+        elif 'tell me a joke' in query:
+            speak(pyjokes.get_joke())
 
+            # basic questions
 
-
-
-
-
-
-
-
+        elif 'how are you' in query:
+            speak("I am fine. Thank you")
