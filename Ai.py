6426edc43b721dd2affb5f9aaa0ec52444bf8a40
RRansubhe <94738575+RRansubhe@users.noreply.github.com>
2021-11-30 21:21:28 +0530

    Update Ai.py

--- Ai.py
+++ Ai.py
@@ -1,10 +1,18 @@
 import datetime
+import os
 import smtplib
 import webbrowser
 import pyjokes
 import pyttsx3
 import speech_recognition as sr
 import wikipedia
+import subprocess
+import ctypes
+import winshell
+import wolframalpha
+import win32com.client as wincl
+import time
+import requests
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
@@ -43,16 +51,15 @@ def takeCommand():
             query = r.recognize_google(audio, language='en-in')
             print(f"User said: {query}\n")  # User query will be printed.
 
-        except Exception as e:
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service{0}".format(e))
+        except Exception:
             # print(e)
             # Say that again will be printed in case of improper voice
             print("Say that again please...")
             takeCommand()
-        except sr.UnknownValueError:
-            print("Google Speech Recognition could not understand audio")
-
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service{0}".format(e))
 
         if query:
             return query.lower()
@@ -69,9 +76,14 @@ def sendEmail(to, content):
     server.close()
 
 
+def uropen():
+    pass
+
+
 if __name__ == "__main__":
     wishMe()
-    while True:
+    # while True:
+    if 1:
         query = takeCommand()
 
         if 'wikipedia' in query:
@@ -109,3 +121,93 @@ if __name__ == "__main__":
 
         elif 'how are you' in query:
             speak("I am fine. Thank you")
+            print("I am fine. Thank you")
+            speak("How are you")
+
+        elif "good" in query:
+            speak("Its good to know that you are fine")
+
+        elif "Who are you" in query:
+            speak("I am your assistant. I can do many things like, open google.")
+
+        elif "Who made you" in query:
+            speak("My sir Raman Ransubhe made me")
+
+            # subprocesses
+
+        elif "lock windows" in query:
+            speak("locking Windows...")
+            ctypes.windll.user32.lockWorkStation()
+
+        elif "shutdown windows" in query:
+            speak("shutting down Windows...")
+            subprocess.call('shutdown / p /f')
+
+        elif "hibernate windows" in query:
+            speak("hibernating Windows...")
+            subprocess.call('shutdown / h')
+
+        elif "restart" in query:
+            speak("restarting Windows...")
+            subprocess.call(["shutdown", "/r"])
+
+        elif "write a note" in query:
+            speak("what should I write")
+            note = takeCommand()
+            file = open('data.txt', 'w')
+            speak("sir, should i include date and time")
+            snfm = takeCommand()
+            if 'yes' in snfm or 'sure' in snfm:
+                strtime = datetime.datetime.now().strftime("% H:% M:% S")
+                file.write(strtime)
+                file.write(" :- ")
+                file.write(note)
+            else:
+                file.write(note)
+
+        elif "show me my notes" in query:
+            speak("showing your notes")
+            file = open("data.txt", "r")
+            print(file.read())
+            speak(file.read(6))
+
+
+
+
+
+        elif 'news' in query:
+            # noinspection PyBroadException
+            try:
+                uropen()
+                data = jason.load(jsonObj)
+                i = 1
+
+                speak("here are your top news form times of India")
+                print('''==============TIMES OF INDIA''' + '\n')
+                for item in datetime.date['articles']:
+                    print(str(i) + '.' + item['tittle'] + '\n')
+                    print(item['description'] + '\n')
+                    speak(str(i) + '.' + item['tittle'] + '\n')
+
+            except Exception:
+                print(str(e))
+
+        elif 'find' in query:
+            app_id = "APP ID"
+            clint = wolframalpha.Client(app_id)
+            indx = query.lower().split().index('find')
+            query = query.split()[indx + 1:1]
+            res = clint.query(' '.join(query))
+            answer = next(res.results).txt
+            print("the answer is" + answer)
+            speak("the answer is" + answer)
+
+        elif 'calculate' in query:
+            app_id = "APP ID"
+            clint = wolframalpha.Client(app_id)
+            indx = query.lower().split().index('calculate')
+            query = query.split()[indx + 1:1]
+            res = clint.query(' '.join(query))
+            answer = next(res.results).txt
+            print("the answer is" + answer)
+            speak("the answer is" + answer)
